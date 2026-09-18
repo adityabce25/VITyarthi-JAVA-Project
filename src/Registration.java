cb703964615fd91df8fafae0bcdc3d66f787ee04
adityabce25 <aditya.25bce11161@vitbhopal.ac.in>
2026-09-18 22:27:25 +0530

    Update Registration.java

--- src/Registration.java
+++ src/Registration.java
@@ -1,2 +1,22 @@
 package src;
-public class Registration { private int eventId; private String student,email; public Registration(int id,String s,String e){eventId=id;student=s;email=e;} public int getEventId(){return eventId;} public String toString(){return eventId+" | "+student+" | "+email;}}
+
+public class Registration {
+    private int eventId;
+    private String studentName;
+    private String email;
+
+    public Registration(int eventId, String studentName, String email) {
+        this.eventId = eventId;
+        this.studentName = studentName;
+        this.email = email;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    @Override
+    public String toString() {
+        return eventId + " | " + studentName + " | " + email;
+    }
+}
